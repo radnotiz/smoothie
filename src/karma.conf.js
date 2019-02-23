fc4fc817e19e21d91528bd60dd291c8a6b408b40
radnotiz <2194747+radnotiz@users.noreply.github.com>
2019-02-23 13:05:50 +0100

    Add continuous integration

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -27,6 +27,12 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    }
   });
 };
