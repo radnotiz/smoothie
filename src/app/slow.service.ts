4c2bf4ec48ee0411961ce7355851e23c71619e53
radnotiz <2194747+radnotiz@users.noreply.github.com>
2019-02-23 22:45:06 +0100

    Add cache with size limit

--- src/app/slow.service.ts
+++ src/app/slow.service.ts
@@ -1,21 +1,55 @@
 import { Injectable } from '@angular/core';
-import { of, Observable } from 'rxjs';
-import { delay, map } from 'rxjs/operators'
+import { BehaviorSubject, of, ReplaySubject, Subject } from 'rxjs';
+import { delay, map, tap } from 'rxjs/operators';
+
+
+export interface AppRequest {
+  query: string,
+  progress: Subject<number>,
+  result: Subject<string>,
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SlowService {
 
+  cache: Map<string, AppRequest> = new Map<string, AppRequest>();
+  cacheSizeLimit = 3;
+
   constructor() { }
 
-  get(request): Observable<any> {
-    return of(request).pipe(
+  get(request: AppRequest): Subject<AppRequest> {
+    const withKey = request.query;
+    return this.cache.has(withKey) ? this.getCache(request, withKey) : this.setCache(request, withKey);
+  }
+
+  setCache(request: AppRequest, withKey: string) {
+    request.result = this.getBackend(request);
+    if (this.cache.size >= this.cacheSizeLimit) {
+      this.cache.delete(this.cache.keys().next().value);
+    }
+    this.cache.set(withKey, request);
+
+    return new BehaviorSubject(request);
+  }
+
+  getCache(request: AppRequest, withKey: string): Subject<AppRequest> {
+    const cached = this.cache.get(withKey)
+    cached.progress.subscribe(progress => request.progress.next(progress));
+    cached.result.subscribe(result => request.result.next(result));
+
+    return new BehaviorSubject(request);
+  }
+
+  getBackend(request: AppRequest): Subject<string> {
+    const result = new ReplaySubject<string>(1);
+    of(request).pipe(
+      tap(() => request.progress.next(0)),
       delay(3000),
-      map((request) => {
-        request.result = 'OK'; //[...request.query].reverse().join('');
-        return request;
-      }),
-    )
+      tap(() => request.progress.next(100)),
+      map(() => 'OK'),
+    ).subscribe(result);
+    return result;
   }
 }
